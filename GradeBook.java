0cea628511f47c2a6b86b640e280c3a2c7a411c1
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-06-25 01:32:21 +0300

    GradeBook

---
+++ GradeBook.java
@@ -0,0 +1,10 @@
+package displaymessage;
+
+public class GradeBook {
+	
+	public void displayMessage(String courseName) {
+		System.out.printf("Welcome to the gradebook for %s!\n", courseName);
+	
+	}
+
+}
